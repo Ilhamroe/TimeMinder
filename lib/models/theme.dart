b47bdd42b42c553456e8395ce9a2d3f10b8ff6c1
LeoZhe <leodyzelvon511@gmail.com>
2024-03-31 22:21:42 +0700

    timer connected model list_timer

---
+++ lib/models/theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+// warna timer
+Color light_blue = const Color.fromRGBO(89, 167, 242, 1);
+Color yellow_timer = const Color.fromRGBO(255, 191, 28, 1);
+Color red_timer = const Color.fromRGBO(250, 62, 62, 1);
+Color app_background = const Color.fromRGBO(239, 245, 251, 1);
+Color tulisan = const Color.fromRGBO(3, 10, 48, 1);
---
+++ lib/pages/timer_view.dart
@@ -0,0 +1,302 @@
+import 'dart:async';
+import 'dart:ui';
+
+import 'package:flutter/painting.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:circular_countdown_timer/circular_countdown_timer.dart';
+import 'package:mobile_time_minder/database/db_helper.dart';
+import 'package:mobile_time_minder/models/theme.dart';
+import 'package:mobile_time_minder/services/homepage.dart';
+import 'package:mobile_time_minder/models/list_timer.dart';
+
+class TimerView extends StatefulWidget {
+  final int timerIndex;
+
+  const TimerView({Key? key, required this.timerIndex}) : super(key: key);
+
+  @override
+  State<TimerView> createState() => _TimerState();
+}
+
+class _TimerState extends State<TimerView> {
+  late Timer _timer;
+
+
+  late int timeInSec;
+ late String _waktuMentah;
+ late String _judul;
+ late String _deskripsi;
+  late int _jam;
+  late int _menit;
+  late int _detik;
+  bool isStarted = false;
+  int focusedMins = 0;
+
+
+  @override
+  void initState() {
+    super.initState();
+    _getDataByID();
+   _convertTimeInSec(context, _jam, _menit, _detik);
+  }
+
+  void _getDataByID(){
+    _timer = Timerlist[widget.timerIndex];
+    _waktuMentah = _timer.time;
+    _judul = _timer.title;
+    _deskripsi = _timer.description;
+    _parseWaktuMentah(_waktuMentah);
+  }
+
+  void _parseWaktuMentah(String time) {
+    List<String> bagian  = time.split(':');
+    _jam = int.parse(bagian[0]);
+    _menit = int.parse(bagian[1]);
+    _detik = int.parse(bagian[2]);
+  }
+
+
+  void _convertTimeInSec(BuildContext context, jam, menit, detik){
+    setState(() {
+      timeInSec =  jam * 3600 + menit * 60 + detik;
+    });
+  }
+  final CountDownController _controller = CountDownController();
+  void startTimer(){
+      const onesec = Duration(seconds: 1);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(),
+        title:Column(
+          children: [
+            SizedBox(height: 20),
+            Text(
+              _judul,
+              style: TextStyle(),
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              _deskripsi,
+              style: TextStyle(fontSize: 14, color: Colors.black), // Atur gaya teks deskripsi
+            ),
+          ],
+        ),
+        centerTitle: true,
+      ),
+      body: SafeArea(
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          padding: const EdgeInsets.symmetric(
+            horizontal: 24,
+            vertical: 100,
+          ),
+
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                CircularCountDownTimer(
+                  duration: timeInSec,
+                  initialDuration: 0,
+                  width: MediaQuery.of(context).size.width / 2,
+                  height: MediaQuery.of(context).size.height / 2,
+                  controller: _controller,
+                  ringColor: app_background,
+                  fillColor: _controller.isPaused ? red_timer : yellow_timer,
+                  strokeWidth: 20.0,
+                  isReverse: true,
+                  isReverseAnimation: true,
+                  strokeCap: StrokeCap.round,
+                  autoStart: true,
+                  textStyle: TextStyle(
+                    fontSize: 33.0,
+                    color: _controller.isPaused ? red_timer : tulisan,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  onComplete: (){
+                    // _showPopupEnd();
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                        builder: (context) => const Homepage(),
+                        )
+                    );
+                  }
+                    // Tindakan yang diambil ketika timer selesai
+                ),
+
+                SizedBox(height: 20),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    if (isStarted)
+                      GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            _controller.resume() ;
+                            isStarted = false;
+                          });
+                        },
+                        child: Icon(
+                          Icons.play_arrow_outlined,
+                          color: light_blue,
+                          size: 40, // Mengatur ukuran ikon menjadi 40
+                        ),
+                      ),
+                    if (!isStarted)
+                      GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            _controller.pause();
+                            isStarted = true;
+                          });
+                        },
+                        child: Icon(
+                          Icons.pause,
+                          color: light_blue,
+                          size: 40, // Mengatur ukuran ikon menjadi 40
+                        ),
+                      ),
+                    SizedBox(width: 100),
+                    IconButton(
+                      onPressed: _showPopup,
+                      icon: Icon(Icons.check),
+                      color: light_blue,
+                      iconSize: 40, // Mengatur ukuran ikon menjadi 40
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+  void _showPopupEnd(){
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Center(
+            child: Icon (Icons.add ,),
+          ),
+          content: Column(
+            mainAxisSize: MainAxisSize.min, // Menentukan ukuran minimum untuk Column
+            children: <Widget>[
+              SizedBox(height: 30),
+              Center(
+                child: Text(
+                  "Kembali ke Beranda ?",
+                  textAlign: TextAlign.center, // Mengatur teks menjadi di tengah
+                ),
+              ),
+            ],
+          ),
+          actions: [
+            SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget> [
+                ElevatedButton(
+
+                  onPressed:(){
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const Homepage(),
+                        )
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: yellow_timer, // Gunakan warna dari variabel state
+                  ),
+                  child: Text("Oke"),
+                )
+              ],
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showPopup() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Center(
+            child: Icon (Icons.add ,),
+          ),
+          content: Column(
+            mainAxisSize: MainAxisSize.min, // Menentukan ukuran minimum untuk Column
+            children: <Widget>[
+              SizedBox(height: 30),
+              Center(
+                child: Text(
+                  "Apakah Anda Yakin ?",
+                  textAlign: TextAlign.center, // Mengatur teks menjadi di tengah
+                ),
+              ),
+            ],
+          ),
+          actions: [
+            SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget> [
+                ElevatedButton(
+                  onPressed:(){
+                    Navigator.of(context).pop();
+                  } ,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.grey, // Gunakan warna dari variabel state
+                  ),
+                  child: Text(
+                      "Tidak",
+                    style: TextStyle(
+                      backgroundColor: Colors.grey ,
+                      color: Colors.white ,
+                    ),
+                  ),
+                ),
+                SizedBox(width: 30),
+                ElevatedButton(
+                  onPressed:(){
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                        builder: (context) => const Homepage(),
+                        )
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: yellow_timer, // Gunakan warna dari variabel state
+                  ),
+                  child: Text("Iya",
+                    style: TextStyle(
+                      backgroundColor: yellow_timer,
+                      color: Colors.white,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
