a103eb7b523af36cde8aee4a6af6ac95199b2305
LeoZhe <leodyzelvon511@gmail.com>
2024-05-02 16:22:33 +0700

    p

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_time_minder/pages/detail_log.dart';
+import 'package:mobile_time_minder/pages/home_page.dart';
 import 'package:mobile_time_minder/services/onboarding_routes.dart';
 import 'package:permission_handler/permission_handler.dart';
 
@@ -28,7 +30,8 @@ class _MainAppState extends State<MainApp> {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       initialRoute: AppRoutes.splash,
-      onGenerateRoute: AppRoutes.generateRoute,
+      // onGenerateRoute: AppRoutes.generateRoute,
+      home: DetailLog(),
     );
   }
 }
