6be5f893041099e9cc97eb10b8b384604cce844c
iramadani2003@gmail.com <iramadani2003@gmail.com>
2024-05-11 14:54:47 +0700

    hmmm

--- lib/widgets/card_home.dart
+++ lib/widgets/card_home.dart
@@ -10,49 +10,9 @@ class CardHome extends StatefulWidget {
 }
 
 class _CardHomeState extends State<CardHome> {
-  bool isLoading = false;
-  late List<Map<String, dynamic>> _allLogData = [];
-  num totalPassed = 0;
-
-  Future<void> _refreshLogData() async {
-    setState(() {
-      isLoading = true;
-    });
-    final List<Map<String, dynamic>> logData = await SQLLogger.getAllData();
-    // print('Value of _allLogData after refresh: $_allLogData');
-    setState(() {
-      _allLogData = logData;
-      isLoading = false;
-    });
-    _totalPassed();
-  }
-
-  void _totalPassed() {
-    if (_allLogData.isNotEmpty) {
-      for (int i = 0; i < _allLogData.length; i++) {
-        if (_allLogData[i]['passed'] != null) {
-          totalPassed += _allLogData[i]['passed'];
-        }
-      }
-    }
-  }
-
-  String _formatTimeLog(int time) {
-    int hours = time ~/ 60;
-    int minutes = time % 60;
-    int seconds = 0;
-
-    String _padLeft(int value) {
-      return value.toString().padLeft(2, '0');
-    }
-
-    return '${_padLeft(hours)} jam ${_padLeft(minutes)} menit ${_padLeft(seconds)} detik';
-  }
-
   @override
   void initState() {
     super.initState();
-    _refreshLogData();
   }
 
   @override
@@ -82,51 +42,28 @@ class _CardHomeState extends State<CardHome> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  _allLogData.isEmpty
-                      ? Column(
-                          children: [
-                            Text(
-                              'Oops! Sepertinya kamu belum\n memulai timer hari ini',
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                  fontFamily: 'Nunito',
-                                  fontSize: screenSize.width * 0.05,
-                                  fontWeight: FontWeight.w500,
-                                  height: 1.5,
-                                  color: pureWhite),
-                            ),
-                            Text(
-                              'Yuk, mulai sekarang!',
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                  fontFamily: 'Nunito-Bold',
-                                  fontSize: screenSize.width * 0.067,
-                                  color: pureWhite),
-                            )
-                          ],
-                        )
-                      : Column(
-                          children: [
-                            Text(
-                              'Hari ini kamu sudah fokus',
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                  fontFamily: 'Nunito',
-                                  fontSize: screenSize.width * 0.05,
-                                  fontWeight: FontWeight.w500,
-                                  height: 1.5,
-                                  color: pureWhite),
-                            ),
-                            Text(
-                              _allLogData[index]['passed'],
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                  fontFamily: 'Nunito-Bold',
-                                  fontSize: screenSize.width * 0.067,
-                                  color: pureWhite),
-                            ),
-                          ],
-                        ),
+                  Column(
+                    children: [
+                      Text(
+                        'Oops! Sepertinya kamu belum\n memulai timer hari ini',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontFamily: 'Nunito',
+                            fontSize: screenSize.width * 0.05,
+                            fontWeight: FontWeight.w500,
+                            height: 1.5,
+                            color: pureWhite),
+                      ),
+                      Text(
+                        'Yuk, mulai sekarang!',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontFamily: 'Nunito-Bold',
+                            fontSize: screenSize.width * 0.067,
+                            color: pureWhite),
+                      )
+                    ],
+                  )
                 ],
               ),
             ),
